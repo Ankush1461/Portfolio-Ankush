f122c6567db76b04cb75301b84a095b644313d18
Ankush Karmakar <karmakarankush1461@gmail.com>
2021-12-19 00:45:10 +0530

    project links added

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,4 +1,3 @@
-import NextLink from "next/link";
 import Image from "next/image";
 import { Box, Text, LinkBox, LinkOverlay } from "@chakra-ui/react";
 import { Global } from "@emotion/react";
@@ -21,24 +20,22 @@ export const GridItem = ({ children, href, title, thumbnail }) => (
   </Box>
 );
 
-export const ProjectGridItem = ({ children, id, title, thumbnail }) => (
+export const ProjectGridItem = ({ children, title, thumbnail }) => (
   <Box w="100%" align="center">
-    <NextLink href={`/projects/${id}`}>
-      <LinkBox cursor="pointer">
-        <Image
-          src={thumbnail}
-          alt={title}
-          className="grid-item-thumbnail"
-          placeholder="blur"
-        />
-        <LinkOverlay href={`/projects/${id}`}>
-          <Text mt={2} fontSize={20}>
-            {title}
-          </Text>
-        </LinkOverlay>
-        <Text fontSize={14}>{children}</Text>
-      </LinkBox>
-    </NextLink>
+    <LinkBox cursor="pointer">
+      <Image
+        src={thumbnail}
+        alt={title}
+        className="grid-item-thumbnail"
+        placeholder="blur"
+      />
+
+      <Text mt={2} fontSize={20}>
+        {title}
+      </Text>
+
+      <Text fontSize={14}>{children}</Text>
+    </LinkBox>
   </Box>
 );
 export const GridItemStyle = () => (
--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,11 @@
-import { Container,  Heading, SimpleGrid } from "@chakra-ui/react"
-import {ProjectGridItem} from "../components/grid-item"
-import ParticleBackground from "../components/ParticleBackground"
-import Section from "../components/section"
+import { Container, Heading, SimpleGrid, Link } from "@chakra-ui/react";
+import { ProjectGridItem } from "../components/grid-item";
+import ParticleBackground from "../components/ParticleBackground";
+import Section from "../components/section";
 import Layout from "../components/layouts/article";
 
-import ThumbInsta from "../public/images/projects/instagram-next-2.png"
-import ThumbUber from "../public/images/projects/uberclone.png"
+import ThumbInsta from "../public/images/projects/instagram-next-2.png";
+import ThumbUber from "../public/images/projects/uberclone.png";
 const Projects = () => {
   return (
     <Layout>
@@ -16,33 +16,38 @@ const Projects = () => {
         </Heading>
         <SimpleGrid columns={[1, 1, 2]} gap={6}>
           <Section>
-            <ProjectGridItem
-              id="instagramnext2"
-              title="Instagram Next 2.0"
-              thumbnail={ThumbInsta}
-            >
-              An Instagram clone where one can <b>post images</b>,{" "}
-              <b>like posts</b> and
-              <b>add comments</b>. Made with technologies like <b>ReactJS</b> ,{" "}
-              <b>NextJS</b> , <b>Firebase v9</b> , <b>TailwindCSS</b>.
-            </ProjectGridItem>
+            <Link href="https://instagram-next-2.vercel.app" target="_blank">
+              <ProjectGridItem
+                title="Instagram Next 2.0"
+                thumbnail={ThumbInsta}
+              >
+                An Instagram clone where one can <b>post images</b>,{" "}
+                <b>like posts</b> and
+                <b>add comments</b>. Made with technologies like <b>ReactJS</b>{" "}
+                , <b>NextJS</b> , <b>Firebase v9</b> , <b>TailwindCSS</b>.
+              </ProjectGridItem>
+            </Link>
           </Section>
           <Section>
-            <ProjectGridItem
-              id="uberclone"
-              title="Uber Clone"
-              thumbnail={ThumbUber}
+            <Link
+              href="https://uber-next-clone-indol.vercel.app"
+              target="_blank"
             >
-              An Uber clone with <b>route-recognition</b> and{" "}
-              <b>relative pricing model</b>. Made with technologies like{" "}
-              <b>ReactJS</b> , <b>NextJS</b> , <b>Firebase v9</b> ,{" "}
-              <b>TailwindCSS</b>.
-            </ProjectGridItem>
+              <ProjectGridItem
+                title="Uber Clone"
+                thumbnail={ThumbUber}
+              >
+                An Uber clone with <b>route-recognition</b> and{" "}
+                <b>relative pricing model</b>. Made with technologies like{" "}
+                <b>ReactJS</b> , <b>NextJS</b> , <b>Firebase v9</b> ,{" "}
+                <b>TailwindCSS</b>.
+              </ProjectGridItem>
+            </Link>
           </Section>
         </SimpleGrid>
       </Container>
     </Layout>
   );
-}
+};
 
-export default Projects
+export default Projects;
