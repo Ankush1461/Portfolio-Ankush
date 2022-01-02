c1315b21fe8f5013babec98b4195d3c32d41a67e
Ankush Karmakar <karmakarankush1461@gmail.com>
2022-01-02 22:47:42 +0530

    Insta proj updated

--- pages/projects.js
+++ pages/projects.js
@@ -3,26 +3,37 @@ import { ProjectGridItem } from "../components/grid-item";
 import Section from "../components/section";
 import Layout from "../components/layouts/article";
 import ParticleBackground from "./../components/ParticleBackground";
-
+import ThumbInsta from "../public/images/projects/instagram-next-2.png";
 import ThumbPort from "../public/images/projects/portfolio.png";
 import ThumbUber from "../public/images/projects/uberclone.png";
 const Projects = () => {
   return (
     <Layout>
-      <ParticleBackground/>
+      <ParticleBackground />
       <Container>
         <Heading as="h3" fontSize={20} mb={4}>
           Projects
         </Heading>
-        <SimpleGrid columns={[1, 1]} gap={6}>
+        <SimpleGrid columns={[1, 1, 1]} gap={6}>
+          <Section>
+            <Link href="https://instagram-next-2.vercel.app" target="_blank">
+              <ProjectGridItem
+                title="Instagram 2.0 Website"
+                thumbnail={ThumbInsta}
+              >
+                An Instagram 2.0 social media clone, where one can{" "}
+                <b>post images</b>, <b>like posts</b> and <b>comment</b>. Made
+                with technologies like <b>ReactJS</b> , <b>NextJS</b> ,{" "}
+                <b>Firebase V9</b> , <b>TailwindCSS</b>.
+              </ProjectGridItem>
+            </Link>
+          </Section>
           <Section>
             <Link href="https://portfolio-ankush.vercel.app" target="_blank">
-              <ProjectGridItem
-                title="Portfolio Website"
-                thumbnail={ThumbPort}
-              >
-                A Portfolio Website. Made with technologies like <b>ReactJS</b>,<br/>
-                 <b>NextJS</b> , <b>Chakra UI</b> , <b>TailwindCSS</b>.
+              <ProjectGridItem title="Portfolio Website" thumbnail={ThumbPort}>
+                A Portfolio Website. Made with technologies like <b>ReactJS</b>,
+                <br />
+                <b>NextJS</b> , <b>Chakra UI</b> , <b>TailwindCSS</b>.
               </ProjectGridItem>
             </Link>
           </Section>
@@ -31,10 +42,7 @@ const Projects = () => {
               href="https://uber-next-clone-indol.vercel.app"
               target="_blank"
             >
-              <ProjectGridItem
-                title="Uber Clone"
-                thumbnail={ThumbUber}
-              >
+              <ProjectGridItem title="Uber Clone" thumbnail={ThumbUber}>
                 An Uber clone with <b>route-recognition</b> and{" "}
                 <b>relative pricing model</b>. Made with technologies like{" "}
                 <b>ReactJS</b> , <b>NextJS</b> , <b>Firebase v9</b> ,{" "}
