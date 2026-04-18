905d2949562ed66fb1ce1090defe86dd73ddb37b
Ankush <karmakarankush1461@gmail.com>
2026-04-19 02:53:00 +0530

    Deployment error fix.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@splinetool/react-spline'],
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
 };
+
 
 module.exports = nextConfig;
